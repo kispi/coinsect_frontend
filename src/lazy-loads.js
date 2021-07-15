499ec9fe51c071ede9b4130baff74aae6ebd154e
Chris <kispi@naver.com>
2021-07-15 18:29:05 +0900

    fix: remove non-exist script import trials

--- src/lazy-loads.js
+++ src/lazy-loads.js
@@ -4,8 +4,6 @@ const useLazyLoads = () => {
   if (process.env.NODE_ENV !== 'production') return
 
   helpers.dom.loadScript({ url: 'https://apis.google.com/js/platform.js' })
-  helpers.dom.loadScript({ url: '/gtm.js' })
-  helpers.dom.loadScript({ url: '/nreum.js' })
 }
 
 export default useLazyLoads
