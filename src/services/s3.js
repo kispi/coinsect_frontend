60841c9b743362621862d416bec95ccc78766cef
Chris <kispi@naver.com>
2021-10-18 15:25:28 +0900

    fix: put 'community_temp=true' tag when upload image

--- src/services/s3.js
+++ src/services/s3.js
@@ -6,13 +6,16 @@ const s3Service = {
 
     const endpoint = 's3/upload_url'
     try {
-      const url = await $http.get(endpoint, { params: { key: `boards/free_board/${encodeURI(file.name)}` } })
+      const { url, headers } = await $http.get(endpoint, {
+        params: {
+          key: `boards/free_board/${encodeURI(file.name)}`,
+          tagging: encodeURI('community_temp=true'),
+        },
+      })
       await $http.put(url, file, {
         // 나중에 할 일이 생기면 해도 될듯
         // onUploadProgress: e => this.progress.value = e.loaded / e.total,
-        headers: {
-          'Content-Type': file.type,
-        },
+        headers,
       })
       return url.split('?')[0]
     } catch (e) {
