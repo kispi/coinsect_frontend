1361b458e42395f438d2479a650d88c7012218e9
Chris <kispi@naver.com>
2022-01-07 10:46:40 +0900

    fix: change default port to 4000

--- was/server.js
+++ was/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const useSSR = require('./ssr')
 const server = express()
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 4000
 
 useSSR(server)
 
