b037b8ec8cc793b2e81aebd2eadfe354bfbb3d0f
Chris <kispi@naver.com>
2022-01-07 15:27:02 +0900

    fix: add matchingRoute function

--- was/controllers.js
+++ was/controllers.js
@@ -2,15 +2,32 @@ const { default: axios } = require('axios')
 const { useApp } = require('./helpers')
 const useHtmlRenderer = require('./html-renderer')
 
+/*
+  클라이언트 라우트에 정의된 route들 중에 현재 SSR로 요청된 path와 매치되는 route를 찾음.
+  사실 없어도 되긴 하는데, 이걸 사용해서 SSR 응답에 대해 302나 404등의 status 처리를 할 수 있음.
+*/
+const matchingRoute = (routes, currentPath) => {
+  const fragCurrentPath = currentPath.split('/').slice(1)
+
+  return routes.find(route => {
+    const fragPath = route.path.split('/').slice(1)
+    if (fragCurrentPath.every((_, idx) => {
+      const existingPath = fragPath[idx] || ''
+      return fragCurrentPath[idx] === existingPath ||
+        (
+          existingPath.startsWith(':') &&
+          !existingPath.startsWith(':pathMatch(.*)*') // NOT FOUND 페이지의 REGEX는 제외
+        )
+    })) return true
+  })
+}
+
 const handleSSRRequest = async (req, res) => {
   const { app, store, router } = await useApp(req)
   await router.push(req.url)
   await router.isReady()
-
-  // 클라이언트 라우트에 정의된 path들 중에 현재 SSR로 요청된 path와 매치되는게 있는지 체크
-  const existingPaths = router.options.routes.map(o => o.path)
-  const isEligiblePath = existingPaths.includes(req.path)
-  if (!isEligiblePath) {
+  const matched = matchingRoute(router.options.routes, req.path)
+  if (!matched) {
     try {
       const { url } = await axios.get(`short_urls?key=${req.path.substring(1)}`)
       res.status(302)
