ba1d465b828609421ec7e0e6d332646e67cdc71f
Chanho Kim <kispi@naver.com>
2022-01-08 13:29:04 +0900

    fix: change number of fractions for very low prices

--- src/helpers/number.js
+++ src/helpers/number.js
@@ -15,11 +15,8 @@ export default {
 
       let numFracs = 0
       if (Math.abs(converted) < 100) numFracs = 2
-
-      if (baseCurrency === 'usd') {
-        if (Math.abs(converted) < 1) numFracs = 4
-        if (Math.abs(converted) < 0.0001) numFracs = 8
-      }
+      if (Math.abs(converted) < 1) numFracs = 4
+      if (Math.abs(converted) < 0.0001) numFracs = 8
 
       if (converted === 0) numFracs = 2
 
