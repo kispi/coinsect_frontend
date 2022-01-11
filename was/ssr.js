6a911d43164d4028e60351cac4723b477da0b6c2
Chris <kispi@naver.com>
2022-01-11 18:06:05 +0900

    chore: add ads.txt

--- was/ssr.js
+++ was/ssr.js
@@ -9,7 +9,7 @@ const useStaticServing = server => {
   server.use('/css', express.static(path.join(__dirname, '../dist/client', 'css')))
   server.use('/font-awesome', express.static(path.join(__dirname, '../dist/client', 'font-awesome')))
   server.use('/favicon', express.static(path.join(__dirname, '../dist/client', 'favicon')))
-  const rootFiles = ['gtm.js', 'robots.txt', 'sitemap.xml', 'naver048dfb4862180b4025eb9bd6e296c6ec.html']
+  const rootFiles = ['gtm.js', 'ads.txt', 'robots.txt', 'sitemap.xml', 'naver048dfb4862180b4025eb9bd6e296c6ec.html']
   rootFiles.forEach(file => server.use(`/${file}`, express.static(path.join(__dirname, '../dist/client', file))))
 }
 
