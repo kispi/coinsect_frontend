0236d8a1c544b252bcd6360d6650b9f1665af8a7
Chris <kispi@naver.com>
2022-01-13 18:24:09 +0900

    fix: do adsbygoogle in onMounted

--- src/components/app/AdSense.vue
+++ src/components/app/AdSense.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="ad-sense">
-    <component :is="'script'" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3069752836489386" crossorigin="anonymous"></component>
-    <!-- desktop-left-vertical -->
     <ins class="adsbygoogle"
       data-ad-client="ca-pub-3069752836489386"
       :data-ad-slot="dataAdSlot"
       data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
-    <component :is="'script'">
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </component>
+      data-full-width-responsive="true"
+    />
   </div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+
 export default {
   props: {
     dataAdSlot: String,
   },
+  setup() {
+    onMounted(() => {
+      (adsbygoogle = window.adsbygoogle || []).push({})
+    })
+  }
 }
 </script>
