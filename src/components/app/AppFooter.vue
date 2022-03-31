0aa9e9bbfbf7e257f1ff716e28de2b66b3ea7321
Chris <kispi@naver.com>
2022-03-31 19:25:17 +0900

    remove footer word

--- src/components/app/AppFooter.vue
+++ src/components/app/AppFooter.vue
@@ -3,7 +3,6 @@
     <div class="layout-centered">
       <div>coinsect.io는 가상자산들의 실시간 가격, 호가창, 시가총액 등의 정보들을 제공합니다.</div>
       <div>이 사이트에서 제공하는 정보들은 투자에 대한 조언이 아니며, 투자에 대한 모든 책임은 본인에게 있습니다.</div>
-      <div>Add github webhook</div>
       <div class="flex-row flex-between items-end m-t-16">
         <address class="flex-row items-center">
           <i class="fal fa-envelope flex-wrap m-r-4 f-16"/><div><a href="mailto:admin@coinsect.io">admin@coinsect.io</a></div>
