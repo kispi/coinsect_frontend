95843afd5917e4add3cda1fd863b548da298132f
Chris <kispi@naver.com>
2022-04-12 12:46:24 +0900

    fix: add slider to <AudioRecorder/>

--- src/components/app/AudioRecorder.vue
+++ src/components/app/AudioRecorder.vue
@@ -1,7 +1,10 @@
 <template>
   <div
     class="audio-recorder"
-    :class="{'playing-disabled': recording || !audioUrl}">
+    :class="{
+      'playing-disabled': recordStatus.ing || !audioUrl,
+      'recording-disabled': playing,
+    }">
     <audio
       ref="refAudio"
       :src="audioUrl"
@@ -9,26 +12,45 @@
       @pause="onPause"
     />
     <div class="recorder-container">
-      <button
-        @click="onClickRecord"
-        class="btn btn-audio-interface record-pause">
-        <i class="fa" :class="recording ? 'fa-stop' : 'fa-circle'"/>
-      </button>
-      <div class="current-time f-mono">
-        {{ formatPlaybackTime(playbackTime) }}
+      <div class="recorder-border overlay a"/>
+      <div class="recorder-border overlay b"/>
+      <div class="recorder-border overlay c"/>
+      <div class="stats">
+        <i
+          @click="onClickRecord"
+          class="fa record-pause"
+          :class="recordStatus.ing ? 'fa-stop' : 'fa-circle'"
+        />
+        <div class="current-time f-mono flex-fill">
+          {{ formatPlaybackTime(playbackTime) }}
+        </div>
+        <i
+          @click="onClickDownload"
+          class="fal download fa-download flex-wrap"
+        />
+        <i
+          @click="onClickPlay"
+          class="fa play-stop"
+          :class="playing ? 'fa-pause' : 'fa-play'"
+        />
       </div>
-      <button
-        v-if="audioUrl"
-        @click="onClickPlay"
-        class="btn btn-audio-interface play">
-        <i class="fa" :class="playing ? 'fa-pause' : 'fa-play'"/>
-      </button>
+      <div class="controls">
+        <div
+          :ref="e => slider.ref = e"
+          class="current-time-slider"
+          @touchstart="onMousedown"
+          @mousedown="onMousedown">
+          <div class="bar background">
+            <div class="bar fill" :style="{ width: `calc(${slider.ratio * 100}%)`}"/>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getCurrentInstance, ref } from 'vue'
+import { getCurrentInstance, ref, onMounted, onUnmounted, watch } from 'vue'
 
 export default {
   setup() {
@@ -36,13 +58,24 @@ export default {
 
     const refAudio = ref(null)
 
+    const slider = ref({
+      ref: null,
+      size: null,
+      ratio: null,
+      nextX: null,
+      dragging: null,
+    })
+
     const interv = ref(null)
 
     const playbackTime = ref(0)
 
     const playing = ref(null)
 
-    const recording = ref(null)
+    const recordStatus = ref({
+      ing: false,
+      interv: null,
+    })
 
     const stream = ref(null)
 
@@ -51,8 +84,6 @@ export default {
     const audioUrl = ref(null)
 
     const media = ref(null)
-
-    const analyser = ref(null)
 
     const lz = t => plugins.$helpers.template.withLeadingZero(t, 2)
 
@@ -73,23 +104,19 @@ export default {
       }
 
       const ctx = new window.AudioContext()
-      analyser.value = ctx.createScriptProcessor(0, 1, 1)
 
       try {
-        const newStream = await navigator.mediaDevices.getUserMedia({ audio: true })
-        const mediaRecorder = new MediaRecorder(newStream, {
+        stream.value = await navigator.mediaDevices.getUserMedia({ audio: true })
+        media.value = new MediaRecorder(stream.value, {
           audioBitsPerSecond: 128000,
         })
-        mediaRecorder.start()
-        stream.value = newStream
-        media.value = mediaRecorder
+        media.value.start()
+        recordStatus.value.ing = true
+        audioUrl.value = null
         source.value = ctx.createMediaStreamSource(stream.value)
-        source.value.connect(analyser.value)
-        analyser.value.connect(ctx.destination)
-        analyser.value.onaudioprocess = e => {
-          recording.value = true
-          playbackTime.value = e.playbackTime
-        }
+        recordStatus.value.interv = setInterval(() => {
+          playbackTime.value = ctx.getOutputTimestamp().contextTime
+        }, 10)
       } catch (e) {
         plugins.$toast.error('녹음 중 알 수 없는 문제가 발생했습니다.')
       }
@@ -97,22 +124,20 @@ export default {
 
     const recordEnd = () => {
       media.value.ondataavailable = e => {
+        clearInterval(recordStatus.value.interv)
+        recordStatus.value.ing = false
         audioUrl.value = URL.createObjectURL(e.data)
-        recording.value = false
       }
 
       stream.value.getAudioTracks().forEach(track => track.stop())
 
       media.value.stop()
 
-      analyser.value.disconnect()
-
       source.value.disconnect()
     }
 
     const onClickRecord = () => {
-      recording.value = !recording.value
-      if (recording.value) recordStart()
+      if (!recordStatus.value.ing) recordStart()
       else recordEnd()
     }
 
@@ -133,15 +158,78 @@ export default {
       else refAudio.value.pause()
     }
 
+    const onClickDownload = () => {
+      const a = document.createElement('a')
+      a.download = true
+      a.href = audioUrl.value
+      a.click()
+    }
+
+    const setNext = e => {
+      if (!slider.value.ref) return
+
+      const rectX =  slider.value.ref.getBoundingClientRect().x
+      slider.value.nextX = (e.touches ? e.touches[0].clientX : e.clientX) - rectX
+
+      if (slider.value.nextX < 0) slider.value.nextX = 0
+      if (slider.value.nextX > slider.value.size) slider.value.nextX = slider.value.size
+
+      slider.value.ratio = slider.value.nextX / slider.value.size
+      refAudio.value.currentTime = refAudio.value.duration * slider.value.ratio
+      playbackTime.value = refAudio.value.currentTime
+    }
+
+    const onMousemove = e => {
+      if (!slider.value.dragging) return
+
+      refAudio.value.pause()
+      setNext(e)
+    }
+
+    const onMousedown = e => {
+      slider.value.dragging = true
+      document.addEventListener('mousemove', onMousemove)
+      document.addEventListener('touchmove', onMousemove)
+
+      setNext(e)
+    }
+
+    const onMouseup = () => {
+      slider.value.dragging = false
+      document.removeEventListener('mousemove', onMousemove)
+      document.removeEventListener('touchmove', onMousemove)
+    }
+
+    onMounted(() => {
+      slider.value.size = slider.value.ref.getBoundingClientRect().width
+      document.addEventListener('mouseup', onMouseup)
+      document.addEventListener('touchend', onMouseup)
+    })
+
+    onUnmounted(() => {
+      document.removeEventListener('mouseup', onMouseup)
+      document.removeEventListener('touchend', onMouseup)
+    })
+
+    watch(
+      () => playbackTime.value,
+      newVal => {
+        slider.value.ratio = newVal / refAudio.value.duration
+      },
+    )
+
     return {
       refAudio,
-      recording,
+      slider,
+      recordStatus,
       playing,
       audioUrl,
       playbackTime,
       onPause,
+      onClickDownload,
       onClickRecord,
       onClickPlay,
+      onMousedown,
       formatPlaybackTime,
     }
   },
@@ -151,44 +239,116 @@ export default {
 <style lang="scss">
 .audio-recorder {
   .recorder-container {
-    width: 196px;
-    overflow: hidden;
-    border-radius: 8px;
-    background: var(--background-light);
-    display: flex;
-    align-items: center;
-    border: 1px solid var(--text-stress);
+    user-select: none;
+    position: relative;
+    width: 320px;
+    color: var(--black);
+    background: var(--white);
+
+    .stats,
+    .controls {
+      display: flex;
+      align-items: center;
+    }
+
+    .stats {
+      padding: 0 8px;
+    }
+
+    .controls {
+      background: var(--white);
+      padding: 0 16px;
+    }
 
     .record-pause,
     .current-time {
       flex: 0 0 auto;
     }
-  }
 
-  .current-time {
-    font-size: 16px;
-    color: var(--text-stress);
-    width: 64px;
-  }
+    .recorder-border {
+      pointer-events: none;
 
-  .btn-audio-interface {
-    background: none;
-    width: 64px;
-    height: 64px;
+      &.a {
+        border: 1px solid red;
+      }
 
-    i {
-      font-size: 24px;
-      color: var(--text-stress);
+      &.b {
+        border: 1px solid green;
+        top: 2px;
+        left: -2px;
+        right: 2px;
+        bottom: -2px;
+      }
 
-      &.fa-circle {
-        color: var(--danger);
+      &.c {
+        border: 1px solid blue;
+        top: -2px;
+        left: 2px;
+        right: -2px;
+        bottom: 2px;
       }
     }
   }
 
+  .current-time {
+    font-size: 16px;
+  }
+
+  .current-time-slider {
+    flex: 1;
+    height: 32px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .fill {
+      height: 100%;
+      width: 50%;
+      background: var(--gs-44);
+      transition: none;
+    }
+
+    .background {
+      position: absolute;
+      overflow: hidden;
+      border-radius: 12px;
+      height: 12px;
+      width: 100%;
+      background: var(--gs-88);
+    }
+  }
+
+  i {
+    width: 48px;
+    height: 48px;
+    font-size: 24px;
+    transition: all 0.2s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+
+    &.fa-circle {
+      color: var(--danger);
+    }
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+
   &.playing-disabled{
-    .app-slider,
-    .btn-audio-interface.play {
+    .current-time-slider,
+    .play-stop,
+    .download {
+      opacity: 0.5;
+      pointer-events: none;
+    }
+  }
+
+  &.recording-disabled {
+    .record-pause {
       opacity: 0.5;
       pointer-events: none;
     }
