d66b881879ce9893838baee7bbddafec25693290
Chris <kispi@naver.com>
2022-07-25 16:50:04 +0900

    fix: use post as http method for crawlMetaTags

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,7 +1,7 @@
 import { $http } from '@/modules/axios'
 
 const helper = {
-  crawlMetaTags: url => $http.get(`helpers/crawled_websites/${encodeURIComponent(url)}`),
+  crawlMetaTags: url => $http.post('helpers/crawled_websites', { url }),
   crawlExampleUrls: () => $http.get('helpers/crawled_websites/examples'),
 }
 
