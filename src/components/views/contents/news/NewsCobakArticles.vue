cec261db0c14b91cf3354e9b78919686f1e4e752
Chanho Kim <kispi@naver.com>
2022-09-27 00:45:09 +0900

    fix: add empty state for <NewsCobakFeeds/>

--- src/components/views/contents/news/NewsCobakArticles.vue
+++ src/components/views/contents/news/NewsCobakArticles.vue
@@ -48,7 +48,7 @@
       <div
         v-if="!loading && data.length === 0"
         class="news-empty center">
-        아직 오늘자({{ params.current_time }}) 뉴스가 올라오지 않았어요 😅
+        아직 오늘자({{ params.current_time }}) 뉴스기사가 올라오지 않았어요 😅
       </div>
       <AppLoader v-if="loading" class="m-a"/>
     </div>
