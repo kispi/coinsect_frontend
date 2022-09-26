80ba3ccbe2d0e4550fd0eacbc751711dc637304d
Chanho Kim <kispi@naver.com>
2022-09-26 21:02:59 +0900

    fix: add condition for ssr

--- src/hooks/addons/pwa.js
+++ src/hooks/addons/pwa.js
@@ -3,13 +3,12 @@ import { useStore } from 'vuex'
 const usePWA = () => {
   const store = useStore()
 
-  const A2HS = window.__A2HS__ || {}
-
-  const firebase = window.__coinsect__firebase__
+  const A2HS = store.getters.isSSR ? {} : (window.__A2HS__ || {})
 
   const initFirebase = async () => {
     if (store.getters.isSSR) return
 
+    const firebase = window.__coinsect__firebase__
     try {
       await firebase.init()
       return firebase.token
