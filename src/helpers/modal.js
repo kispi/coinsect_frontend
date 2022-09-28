e4eaeac7bd1c8cd5d506d1cb6f061a121e65bb6d
Chanho Kim <kispi@naver.com>
2022-09-28 19:44:41 +0900

    fix: null check

--- src/helpers/modal.js
+++ src/helpers/modal.js
@@ -1,7 +1,7 @@
 import { store as $store } from '@/store'
 import translate from './translate'
 
-const isOpened = component => $store.getters.modals.find(modal => modal.component === component) ? true : false
+const isOpened = component => $store.getters.modals.find(modal => (modal || {}).component === component) ? true : false
 
 const initModal = (options, component) => new Promise(resolve => {
   if (
