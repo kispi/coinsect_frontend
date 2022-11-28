e8afdeed7408ae6275ef9c551b57119e85f95698
DESKTOP-QDHV7HA\김찬호 <kispi@naver.com>
2022-11-28 23:36:01 +0900

    fix: use coinsect api for news

--- src/services/content.js
+++ src/services/content.js
@@ -4,6 +4,7 @@ const content = {
   realTimePositions: () => $http.get('contents/real_time_positions'),
   publicTreasuries: () => $http.get('contents/public_treasuries'),
   news: {
+    upbit: () => $http.get('contents/news/upbit'),
     feeds: params => $http.get('contents/news/cobak/feeds', { params }),
     articles: params => $http.get('contents/news/cobak/articles', { params }),
     issues: params => $http.get('contents/news/cobak/issues', { params }),
