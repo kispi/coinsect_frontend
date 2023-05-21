98c23f98af1eb090efcbb6ef40d9e0ae500d5dd0
DESKTOP-QDHV7HA\김찬호 <kispi@naver.com>
2023-05-21 23:02:17 +0900

    fix: null bug

--- src/helpers/logic.js
+++ src/helpers/logic.js
@@ -93,7 +93,7 @@ const logic = {
   hexToRgba: (hex, alpha = 1) => {
     if (!hex) return
 
-    const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16))
+    const [r, g, b] = (hex.match(/\w\w/g) || []).map(x => parseInt(x, 16))
     return `rgba(${r},${g},${b},${alpha})`
   },
   acceptableFileSize: (file, maxFileSize = 5) => {
