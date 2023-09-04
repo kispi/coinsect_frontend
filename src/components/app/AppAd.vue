fa49f2020492e0ae23d498fe59faa88f6c37ff54
Chris <kispi@naver.com>
2023-09-04 14:40:41 +0900

    fix: show <AdSense/> on the right vertical position

--- src/components/app/AppAd.vue
+++ src/components/app/AppAd.vue
@@ -10,6 +10,7 @@
       :key="exchange"
       v-for="exchange in ['bybit', 'bitget', 'bingx']"
     />
+    <AdSense v-if="$store.getters.windowInnerWidth >= 1200" :dataAdSlot="'3927887162'" :responsive="true"/>
   </div>
 </template>
 
@@ -19,5 +20,14 @@
   display: grid;
   gap: 8px;
   grid-template-columns: repeat(3, 1fr);
+
+  --banner-width: 200px;
+  .ad-sense {
+    width: var(--banner-width);
+    height: 600px;
+    position: fixed;
+    top: calc(var(--app-header-height) + var(--app-default-page-padding));
+    right: calc((100vw - 1200px) / 2 - var(--banner-width) - 16px);
+  }
 }
 </style>
