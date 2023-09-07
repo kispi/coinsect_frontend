3dbad97af8934b001c58b5fcdfe39d50f874b17e
DESKTOP-QDHV7HA\김찬호 <kispi@naver.com>
2023-09-08 00:18:09 +0900

    fix: apply bybit fee changes

--- src/assets/constants/referrals.js
+++ src/assets/constants/referrals.js
@@ -1,6 +1,6 @@
 export const referrals = {
   bybit: {
-    promotion: '수수료 <b>20%</b> 할인\n(지정가: 0.01% / 시장가 0.048%)',
+    promotion: '수수료 <b>20%</b> 할인\n(지정가: 0.02% / 시장가 0.044%)',
     travelRule: ['upbit', 'bithumb', 'coinone', 'korbit'],
     benefits: ['입금액 / 거래량에 따라 최대 30,000 USDT 보너스'],
     link: 'https://partner.bybit.com/b/coinsect',
